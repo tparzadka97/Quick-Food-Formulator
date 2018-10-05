3d752051527798b97a7d40e86a24dacbb6d5bd2e
Haleigh Gronwold <hgronwol@purdue.edu>
2018-10-04 20:58:07 -0400

    set up login stuff

--- app/src/main/java/com/example/haleigh/quickfoodformulator/MainActivity.java
+++ app/src/main/java/com/example/haleigh/quickfoodformulator/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.haleigh.quickfoodformulator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.loginDirectionalButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, login.class));
+            }
+        });
     }
+
 }
